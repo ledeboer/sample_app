9f64d2e119e31bd604ff98fbfa19b4bb291db84f
Paul Ledeboer <paul.ledeboer@gmail.com>
2013-11-08 12:01:50 -0600

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
+    else
+      # Create an error message and re-render the signin form.
+      if user
+        flash.now[:error] = 'Invalid email/password combination' 
+      else
+        flash.now[:error] = 'Invalid sign in attempt: That email is not a registered user'
+      end
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+end
